b512f668407e678e9ec29c84564f159b89cc7104
ohoud-habib <ohoudhabib@gmail.com>
2024-10-20 10:35:15 +0300

    Initial Commit

---
+++ secondChallenge/secondChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  secondChallengeApp.swift
+//  secondChallenge
+//
+//  Created by ohoud on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct secondChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
